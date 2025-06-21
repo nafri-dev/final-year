d82a2cb53274e1d0ba0234af2c0e76b82c01f2a2
nafri-dev <mdirfantreasurer@gmail.com>
2025-06-21 20:42:35 +0530

    er

--- project-backend-node/server.js
+++ project-backend-node/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000
 // CORS configuration
 app.use(
   cors({
-    origin: [process.env.VITE_FRONTEND_URL || "http://localhost:5173", "http://localhost:5174"],
+    origin: [process.env.VITE_FRONTEND_URL || "http://localhost:5173", "http://localhost:5174", "https://final-year-q8mx.vercel.app"],
     optionsSuccessStatus: 200,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
