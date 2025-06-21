74f4b1961d290b0654a338e52153fdae542046fd
nafri-dev <mdirfantreasurer@gmail.com>
2025-06-21 20:32:01 +0530

    ER

--- project-frontend/src/api/productApi.js
+++ project-frontend/src/api/productApi.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = import.meta.env.API_URL || "http://localhost:3000/api" 
+const API_BASE_URL = import.meta.env.API_URL
 
 // Update the axiosInstance to include the auth token from localStorage
 const axiosInstance = axios.create({
